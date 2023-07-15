d19c15f0b8f553a3ac7d147630b2dc907e0eedc0
Darksied95 <aliyuefe9501@gmail.com>
2023-07-15 12:57:31 +0100

    finish profile tablet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header/Header";
 import NavBar from "@/components/NavBar/NavBar";
 import Profile from "@/components/Profile/Profile";
-import Repositories from "@/components/Repositories/Repositories";
 import RepositoriesContainer from "@/components/RepositoriesContainer/RepositoriesContainer";
 
 export default function Home() {
@@ -9,7 +8,7 @@ export default function Home() {
     <>
       <Header />
       <NavBar />
-      <main className="bg-body-mobile nav4:bg-body-desktop px-5 pt-12 ">
+      <main className="bg-body-mobile nav4:bg-body-desktop px-5 pt-12 main1:flex main1:px-8 main1:pt-10">
         <Profile />
         <RepositoriesContainer />
       </main>
