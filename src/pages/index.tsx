6b5e261fc4ba81199501d2d219d4f47d600515c5
Darksied95 <aliyuefe9501@gmail.com>
2023-07-20 15:07:27 +0100

    move home page from repo-> sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,9 @@
-import useFetch from "@/apiService/useFetch";
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Main from "@/components/Main/Main";
-import NavBar from "@/components/NavBar/NavBar";
-import { updateGithubProfile } from "@/redux/store";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import SignIn from "@/components/SignIn/SignIn";
 
 export default function Home() {
-  const dispatch = useDispatch();
-
-  const { profileData, repoData, isLoading, starredData } =
-    useFetch("darksied95");
-  useEffect(() => {
-    dispatch(
-      updateGithubProfile({
-        profile: profileData?.data,
-        repos: repoData?.data,
-        loading: isLoading,
-        starCount: starredData,
-      })
-    );
-  }, [dispatch, isLoading, profileData, repoData, starredData]);
-
   return (
-    <>
-      <Header />
-      <NavBar />
-      <Main />
-      <Footer />
-    </>
+    <div>
+      <SignIn />
+    </div>
   );
 }
